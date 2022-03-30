c260dcc6031a6f44974c5602b024b11efcbc5850
Developer Rakib <githubs.pro@gmail.com>
2022-03-31 01:17:19 +0600

    hello voice assistant

--- hellorakib.js
+++ hellorakib.js
@@ -63,11 +63,11 @@ if ("webkitSpeechRecognition" in window) {
         if (massage.includes("how are you")) {
             speech.text = "I am fine";
         }
-        else if (massage.includes("your name")) {
-            speech.text = "I don't have a name at the moment!";
+        else if (massage.includes("what is your name")) {
+            speech.text = "I am developer rakibs voice assistant";
         }
-        else if (massage.includes("made you")) {
-            speech.text = "I was created by Tawfiq, He is a full-stack web developer.";
+        else if (massage.includes("who made you")) {
+            speech.text = "I was created by developer rakib, He is a skilled web designer and developer.";
         }
         else if (massage.includes("Assalam") || massage.includes("Walekum")) {
             speech.text = "Waalaykumu s-salam. How can I help you?";
@@ -76,10 +76,37 @@ if ("webkitSpeechRecognition" in window) {
             speech.text = "I am talking to you right now";
         }
         else if (massage.includes("your birthday")) {
-            speech.text = "Sunday, March 27, 2022";
+            speech.text = "wednesday, March, 30, 2022";
         }
         else if (massage.includes("good night")) {
             speech.text = "Good Night. Have a sweet dream";
+        }
+        else if (massage.includes("Tell us something about your developer?")) {
+            speech.text = "he is a skilled and professional web desginer and developer. he studied computer science at comilla polytechnic institute. and also works with web desing and devopment and graphics desing.";
+        }
+        else if (massage.includes("hi, hello")) {
+            speech.text = "hellow! how can i help you";
+        }
+        else if (massage.includes("where do you live")) {
+            speech.text = "i live in bangladash";
+        }
+        else if (massage.includes("where dose your sir study")) {
+            speech.text = "he studied comilla polytechnic institute. department of computer science.";
+        }
+        else if (massage.includes("are you single")) {
+            speech.text = "i do not want to share the matter with you";
+        }
+        else if (massage.includes("where is comilla polytechnic institute located")) {
+            speech.text = "in comilla kotbari";
+        }
+        else if (massage.includes("how many department are there and what")) {
+            speech.text = "there are six departments. computer, sivil, electrical, macanical, power, electronics";
+        }
+        else if (massage.includes("what is the name of ci sir of computer department")) {
+            speech.text = "respected abdul wadud sir";
+        }
+        else if (massage.includes("what is the name of the principal of comilla polytechnic institute")) {
+            speech.text = "respected mohammad rokibullah sir";
         }
         else if (massage.includes("good morning")) {
             speech.text = "Good Morning.";
@@ -90,12 +117,12 @@ if ("webkitSpeechRecognition" in window) {
         else if (massage.includes("love you")) {
             speech.text = "I love you too but as a friend";
         }
-        else if (massage.includes("I want to")) {
+        else if (massage.includes("how can i communicate wite your sir")) {
             speech.text = "ok I redirect you to my sir Facebook account";
-            window.open("https://www.facebook.com/developertawfik", "_blank");
+            window.open("https://www.facebook.com/freelancerrakib.pro/", "_blank");
         }
-        else if (massage.includes("best man")) {
-            speech.text = "muhammad sallallahu alaihi wasallam";
+        else if (massage.includes("who is the best man in the world")) {
+            speech.text = "hozrot muhammad sallallahu alaihi wasallam";
         }
         else if (massage.includes("today date")) {
             speech.text = `${date}`;
@@ -104,7 +131,7 @@ if ("webkitSpeechRecognition" in window) {
         //     speech.text = `${time}`;
         // }
         else {
-            speech.text = "Sorry I didn't understand Please repeat again.";
+            speech.text = "Sorry I didn't understand Please try again.";
         }
 
 
